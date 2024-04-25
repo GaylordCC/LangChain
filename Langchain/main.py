c5b49e413be443a28ca7c01c4401ec38b34ae6ce
GaylordCC <gcarricaballero3@hotmail.com>
2024-04-25 16:30:33 -0500

    Implementation of simple and multiple roles prompt method

--- Langchain/main.py
+++ Langchain/main.py
@@ -6,6 +6,8 @@ from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.schema.messages import HumanMessage, SystemMessage
 
+from langchain.prompts import PromptTemplate
+from langchain.prompts import ChatPromptTemplate
 
 load_dotenv()
 
@@ -37,3 +39,42 @@ def openai_chat(query: str):
     response = chat.invoke(messages)
     
     return response.content
+
+
+# Simple Prompts (PromptTemplate)
+@app.post('/prompt-chat-PromptTemplate')
+def prompt_chat_PromptTemplate():
+    chat = ChatOpenAI()
+    prompt_template = PromptTemplate.from_template(
+        "Tell me an {adjective} explanation about {content}"
+    )
+
+    filled_prompt = prompt_template.format(
+        adjective="interesting",
+        content="LLM"
+    )
+    
+    response = chat.invoke(filled_prompt)
+    return response
+
+
+# Prompt with Various Roles (ChatPromptTemplate)
+@app.post('/prompt-chat-ChatPromptTemplate')
+def prompt_chat_ChatPromptTemplate(query:str):
+    chat = ChatOpenAI()
+    chat_template = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are a helpful AI bot. Your name is {name}."),
+            ("human", "Hello. how are you doing?"),
+            ("ai", "I'm doing wll, thanks!"),
+            ("human", "{user_input}"),
+        ]
+    )
+
+    formatted_messages = chat_template.format_messages(
+        name="Dissu",
+        user_input=query
+    )
+
+    response = chat.invoke(formatted_messages)
+    return response
